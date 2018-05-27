017297ff94bacf46195308181e895dd5913a420f
piccadi11y <joshuajones3666@gmail.com>
2018-05-27 13:16:01 +1200

    Added game mode for running robot

---
+++ Source/VirtualRobot/Robot_Run_GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Robot_Run_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRTUALROBOT_API ARobot_Run_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
