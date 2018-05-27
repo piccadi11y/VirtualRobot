786643624b4e40fcff05812df0e0927ff5765edf
piccadi11y <joshuajones3666@gmail.com>
2018-05-27 14:35:52 +1200

    Started building code for selection screen when running robot
    - Implemented code for using widgets in the Robot_Run_GameModeBase.
    - Created Robot_Run_PlayerController for running the robot.

--- Source/VirtualRobot/Robot_Run_GameModeBase.h
+++ Source/VirtualRobot/Robot_Run_GameModeBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Blueprint/UserWidget.h"
 #include "Robot_Run_GameModeBase.generated.h"
 
 /**
@@ -14,7 +15,22 @@ class VIRTUALROBOT_API ARobot_Run_GameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
-	
+public:
+	/** Remove the current menu widget and create a new one from the specified class, if provided. */
+	UFUNCTION(BlueprintCallable, Category = Menus)
+		void ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass);
+
+protected:
+	/** Called when the game starts. */
+	virtual void BeginPlay() override;
+
+	/** The widget class we will use as our menu widget when the program start. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Menus)
+		TSubclassOf<UUserWidget> StartingWidgetClass;
+
+	/** The widget instance that we are currently using as our menu. */
+	UPROPERTY()
+		UUserWidget* CurrentWidget;
 	
 	
 };
---
+++ Source/VirtualRobot/Robot_Run_PlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Robot_Run_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRTUALROBOT_API ARobot_Run_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
