242b670581ca065fa426d58c62bb33ae5dfafc74
piccadi11y <joshuajones3666@gmail.com>
2018-05-28 21:56:29 +1200

    Added custom 'FileInterface' class
    Created the class that I'm going to build the file interface functionality in. This class will be included wherever we need to interface with a file.

---
+++ Source/VirtualRobot/FileInterface.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FileHelper.h"
+#include "PlatformFile.h"
+
+/**
+ * 
+ */
+class VIRTUALROBOT_API FileInterface
+{
+public:
+	FileInterface();
+	~FileInterface();
+};
